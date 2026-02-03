bb79c23e3e93dbf749a8cd299c627f9124794ffa
Borys Ziukov <ziukoff1985@gmail.com>
2026-02-03 14:46:32 +0200

    Add Layout and base routing in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Layout from './components/Layout/Layout';
+import HomePage from './pages/HomePage/HomePage';
+import MoviesPage from './pages/Movies/MoviesPage';
+import ActorsPage from './pages/Actors/ActorsPage';
+import DirectorsPage from './pages/Directors/DirectorsPage';
+import StudiosPage from './pages/Studios/StudiosPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path='movies' element={<MoviesPage />} />
+                    <Route path='actors' element={<ActorsPage />} />
+                    <Route path='directors' element={<DirectorsPage />} />
+                    <Route path='studios' element={<StudiosPage />} />
+                    <Route path='*' element={<NotFoundPage />} />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,5 +1,64 @@
+import { Outlet } from 'react-router-dom';
+
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import NavBar from '../NavBar/NavBar';
+import CinemaService from '../ServiceBar/CinemaService';
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 function Layout() {
-    return <div>Layout</div>;
+    return (
+        <Box>
+            <Grid container direction='column'>
+                <Grid
+                    size={{ xs: 12 }}
+                    sx={{
+                        padding: '0px',
+                        backgroundColor: 'gray',
+                        minHeight: '10vh',
+                    }}
+                >
+                    <Header />
+                </Grid>
+                <Grid
+                    container
+                    direction='row'
+                    sx={{ padding: '0px', minHeight: '80vh' }}
+                >
+                    <Grid
+                        size={{ xs: 2 }}
+                        sx={{ padding: '0px', backgroundColor: 'teal' }}
+                    >
+                        <NavBar />
+                    </Grid>
+                    <Grid
+                        size={{ xs: 7 }}
+                        sx={{ padding: '0px', backgroundColor: 'pink' }}
+                    >
+                        <Outlet />
+                    </Grid>
+                    <Grid
+                        size={{ xs: 3 }}
+                        sx={{ padding: '0px', backgroundColor: 'yellow' }}
+                    >
+                        <CinemaService />
+                    </Grid>
+                </Grid>
+                <Grid
+                    size={{ xs: 12 }}
+                    sx={{
+                        padding: '0px',
+                        backgroundColor: 'skyblue',
+                        minHeight: '10vh',
+                    }}
+                >
+                    <Footer />
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
 
 export default Layout;
