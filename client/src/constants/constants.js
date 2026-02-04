9b3310e0296d2caf730f2cd8703bc86cc5237c25
Borys Ziukov <ziukoff1985@gmail.com>
2026-02-04 14:24:40 +0200

    Fix db.json, add empty_entity constants

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -4,3 +4,42 @@ export const MOVIES_SLICE_NAME = 'movies';
 export const ACTORS_SLICE_NAME = 'actors';
 export const DIRECTORS_SLICE_NAME = 'directors';
 export const STUDIOS_SLICE_NAME = 'studios';
+
+export const EMPTY_ACTOR_DATA = {
+    id: null,
+    movies: [],
+    firstName: '',
+    lastName: '',
+    birthDate: '',
+    nationality: '',
+    image: '',
+};
+
+export const EMPTY_DIRECTOR_DATA = {
+    id: null,
+    firstName: '',
+    lastName: '',
+    birthYear: '',
+    nationality: '',
+    movies: [],
+    image: '',
+};
+
+export const EMPTY_STUDIO_DATA = {
+    id: null,
+    title: '',
+    location: '',
+    foundationYear: '',
+    movies: [],
+    logo: '',
+};
+
+export const EMPTY_MOVIE_DATA = {
+    id: null,
+    title: '',
+    directors: [],
+    actors: [],
+    studios: [],
+    releaseYear: '',
+    poster: '',
+};
