c1cd24a022a04010cd5b3e767e3b98b3f1edc937
Borys Ziukov <ziukoff1985@gmail.com>
2026-02-04 14:02:12 +0200

    Add store Provider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App.jsx';
+import store from './store/index.js';
 
 import './index.css';
 import 'modern-normalize';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </StrictMode>,
 );
