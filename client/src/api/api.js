8fe7acd6db170ae1d08d1cc7768e5e67a9fd9998
Borys Ziukov <ziukoff1985@gmail.com>
2026-02-04 13:11:30 +0200

    Add axios instance and constants

---
+++ client/src/api/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+import { BASE_URL } from '../constants/constants';
+
+export const api = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
---
+++ client/src/constants/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'http://localhost:5000';
+
+export const MOVIES_SLICE_NAME = 'movies';
+export const ACTORS_SLICE_NAME = 'actors';
+export const DIRECTORS_SLICE_NAME = 'directors';
+export const STUDIOS_SLICE_NAME = 'studios';
