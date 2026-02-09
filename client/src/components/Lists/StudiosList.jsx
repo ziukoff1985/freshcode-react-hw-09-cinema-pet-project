5d755808b38a68657afac9398fc2ad2db2aa682b
Borys Ziukov <ziukoff1985@gmail.com>
2026-02-09 14:47:58 +0200

    Fix deleting logic in StudiosList using ConfirmDrawer and useConfirm

--- client/src/components/Lists/StudiosList.jsx
+++ client/src/components/Lists/StudiosList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import {
@@ -14,9 +14,6 @@ import {
     Box,
     Tooltip,
     CircularProgress,
-    Drawer,
-    Button,
-    Stack,
 } from '@mui/material';
 import {
     Visibility as ViewIcon,
@@ -25,28 +22,10 @@ import {
     Person as PersonIcon,
 } from '@mui/icons-material';
 import { deleteStudio, getAllStudios } from '../../store/thunks/studiosThunks';
+import useConfirm from '../../hooks/useConfirm';
+import ConfirmDrawer from '../UI/ConfirmDrawer';
 
 function StudiosList() {
-    const [openConfirm, setOpenConfirm] = useState(false);
-    const [selectedStudioId, setSelectedStudioId] = useState(null);
-
-    const handleOpenDeleteConfirm = (id) => {
-        setOpenConfirm(true);
-        setSelectedStudioId(id);
-    };
-
-    const handleCloseDeleteConfirm = () => {
-        setOpenConfirm(false);
-        setSelectedStudioId(null);
-    };
-
-    const handleDelete = () => {
-        if (selectedStudioId) {
-            dispatch(deleteStudio(selectedStudioId));
-        }
-        handleCloseDeleteConfirm();
-    };
-
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { studios, isPending } = useSelector((state) => state.studiosList);
@@ -54,6 +33,17 @@ function StudiosList() {
     useEffect(() => {
         dispatch(getAllStudios());
     }, [dispatch]);
+
+    const confirm = useConfirm();
+
+    const handleDeleteClick = (id) => {
+        confirm.openConfirm(id);
+    };
+
+    const handleConfirmDelete = () => {
+        dispatch(deleteStudio(confirm.payload));
+        confirm.closeConfirm();
+    };
 
     if (!studios || isPending)
         return (
@@ -103,9 +93,7 @@ function StudiosList() {
                                         <Tooltip title='Delete'>
                                             <IconButton
                                                 onClick={() =>
-                                                    handleOpenDeleteConfirm(
-                                                        studio.id,
-                                                    )
+                                                    handleDeleteClick(studio.id)
                                                 }
                                                 color='error'
                                             >
@@ -157,44 +145,15 @@ function StudiosList() {
                 </List>
             </Paper>
 
-            <Drawer
-                anchor='right' // або 'bottom'
-                open={openConfirm}
-                onClose={handleCloseDeleteConfirm}
-            >
-                <Box
-                    sx={{
-                        width: { xs: '100vw', sm: 360 },
-                        p: 3,
-                    }}
-                >
-                    <Typography variant='h6' gutterBottom>
-                        ❗ Warning
-                    </Typography>
-
-                    <Typography variant='body2' color='text.secondary' mb={3}>
-                        Are you sure you want to delete? This action cannot be
-                        undone.
-                    </Typography>
-
-                    <Stack
-                        direction='row'
-                        spacing={2}
-                        justifyContent='flex-end'
-                    >
-                        <Button onClick={handleCloseDeleteConfirm}>
-                            Cancel
-                        </Button>
-                        <Button
-                            onClick={handleDelete}
-                            variant='contained'
-                            color='error'
-                        >
-                            Delete
-                        </Button>
-                    </Stack>
-                </Box>
-            </Drawer>
+            <ConfirmDrawer
+                open={confirm.open}
+                title='Delete studio'
+                description='Are you sure you want to delete studio? This action cannot be undone.'
+                confirmText='Delete'
+                cancelText='Cancel'
+                onConfirm={handleConfirmDelete}
+                onClose={confirm.closeConfirm}
+            />
         </>
     );
 }
