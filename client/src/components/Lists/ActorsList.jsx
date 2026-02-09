e9252a98682796d24ce4e49363afe2891e214d04
Borys Ziukov <ziukoff1985@gmail.com>
2026-02-09 13:34:04 +0200

    Add Drawer (delete confirmation) to ActorsList

--- client/src/components/Lists/ActorsList.jsx
+++ client/src/components/Lists/ActorsList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import {
@@ -14,6 +14,9 @@ import {
     Box,
     Tooltip,
     CircularProgress,
+    Drawer,
+    Button,
+    Stack,
 } from '@mui/material';
 import {
     Visibility as ViewIcon,
@@ -24,6 +27,9 @@ import {
 import { getAllActors, deleteActor } from '../../store/thunks/actorsThunks';
 
 function ActorsList() {
+    const [openConfirm, setOpenConfirm] = useState(false);
+    const [selectedActorId, setSelectedActorId] = useState(null);
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { actors, isPending } = useSelector((state) => state.actorsList);
@@ -32,10 +38,21 @@ function ActorsList() {
         dispatch(getAllActors());
     }, [dispatch]);
 
-    const handleDelete = (id) => {
-        if (window.confirm('Are you sure you want to delete this actor?')) {
-            dispatch(deleteActor(id));
+    const handleOpenDeleteConfirm = (actorId) => {
+        setOpenConfirm(true);
+        setSelectedActorId(actorId);
+    };
+
+    const handleCloseDeleteConfirm = () => {
+        setOpenConfirm(false);
+        setSelectedActorId(null);
+    };
+
+    const handleDelete = () => {
+        if (selectedActorId) {
+            dispatch(deleteActor(selectedActorId));
         }
+        handleCloseDeleteConfirm();
     };
 
     if (!actors || isPending)
@@ -46,90 +63,137 @@ function ActorsList() {
         );
 
     return (
-        <Paper
-            elevation={2}
-            sx={{ width: '100%', bgcolor: 'background.paper', borderRadius: 2 }}
-        >
-            <List sx={{ width: '100%', py: 0 }}>
-                {actors.map((actor, index) => (
-                    <Box key={actor.id}>
-                        <ListItem
-                            alignItems='flex-start'
-                            secondaryAction={
-                                <Box>
-                                    <Tooltip title='Show details'>
-                                        <IconButton
-                                            component={Link}
-                                            to={`${actor.id}`}
-                                            color='primary'
-                                        >
-                                            <ViewIcon />
-                                        </IconButton>
-                                    </Tooltip>
-                                    <Tooltip title='Edit'>
-                                        <IconButton
-                                            onClick={() =>
-                                                navigate(`${actor.id}/edit`)
-                                            }
-                                            color='secondary'
-                                        >
-                                            <EditIcon />
-                                        </IconButton>
-                                    </Tooltip>
-                                    <Tooltip title='Delete'>
-                                        <IconButton
-                                            onClick={() =>
-                                                handleDelete(actor.id)
-                                            }
-                                            color='error'
-                                        >
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </Tooltip>
-                                </Box>
-                            }
-                        >
-                            <ListItemAvatar>
-                                <Avatar
-                                    alt={`${actor.firstName} ${actor.lastName}`}
-                                    src={actor.image}
-                                    sx={{ width: 50, height: 50, mr: 2 }}
-                                >
-                                    <PersonIcon />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary={
-                                    <Typography
-                                        sx={{
-                                            fontWeight: 'bold',
-                                            fontSize: '16px',
-                                        }}
+        <>
+            <Paper
+                elevation={2}
+                sx={{
+                    width: '100%',
+                    bgcolor: 'background.paper',
+                    borderRadius: 2,
+                }}
+            >
+                <List sx={{ width: '100%', py: 0 }}>
+                    {actors.map((actor, index) => (
+                        <Box key={actor.id}>
+                            <ListItem
+                                alignItems='flex-start'
+                                secondaryAction={
+                                    <Box>
+                                        <Tooltip title='Show details'>
+                                            <IconButton
+                                                component={Link}
+                                                to={`${actor.id}`}
+                                                color='primary'
+                                            >
+                                                <ViewIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                        <Tooltip title='Edit'>
+                                            <IconButton
+                                                onClick={() =>
+                                                    navigate(`${actor.id}/edit`)
+                                                }
+                                                color='secondary'
+                                            >
+                                                <EditIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                        <Tooltip title='Delete'>
+                                            <IconButton
+                                                onClick={() =>
+                                                    handleOpenDeleteConfirm(
+                                                        actor.id,
+                                                    )
+                                                }
+                                                color='error'
+                                            >
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                    </Box>
+                                }
+                            >
+                                <ListItemAvatar>
+                                    <Avatar
+                                        alt={`${actor.firstName} ${actor.lastName}`}
+                                        src={actor.image}
+                                        sx={{ width: 50, height: 50, mr: 2 }}
                                     >
-                                        {`${actor.firstName} ${actor.lastName}`}
-                                    </Typography>
-                                }
-                                secondary={
-                                    <>
+                                        <PersonIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={
                                         <Typography
-                                            component='span'
-                                            variant='body2'
-                                            color='text.primary'
+                                            sx={{
+                                                fontWeight: 'bold',
+                                                fontSize: '16px',
+                                            }}
                                         >
-                                            {actor.nationality}
+                                            {`${actor.firstName} ${actor.lastName}`}
                                         </Typography>
-                                        {` — Birth date: ${actor.birthDate}`}
-                                    </>
-                                }
-                            />
-                        </ListItem>
-                        {index < actors.length - 1 && (
-                            <Divider variant='inset' component='li' />
-                        )}
-                    </Box>
-                ))}
-            </List>
-        </Paper>
+                                    }
+                                    secondary={
+                                        <>
+                                            <Typography
+                                                component='span'
+                                                variant='body2'
+                                                color='text.primary'
+                                            >
+                                                {actor.nationality}
+                                            </Typography>
+                                            {` — Birth date: ${actor.birthDate}`}
+                                        </>
+                                    }
+                                />
+                            </ListItem>
+                            {index < actors.length - 1 && (
+                                <Divider variant='inset' component='li' />
+                            )}
+                        </Box>
+                    ))}
+                </List>
+            </Paper>
+
+            <Drawer
+                anchor='right' // або 'bottom'
+                open={openConfirm}
+                onClose={handleCloseDeleteConfirm}
+            >
+                <Box
+                    sx={{
+                        width: { xs: '100vw', sm: 360 },
+                        p: 3,
+                    }}
+                >
+                    <Typography variant='h6' gutterBottom>
+                        Delete movie
+                    </Typography>
+
+                    <Typography variant='body2' color='text.secondary' mb={3}>
+                        Are you sure you want to delete this actor? This action
+                        cannot be undone.
+                    </Typography>
+
+                    <Stack
+                        direction='row'
+                        spacing={2}
+                        justifyContent='flex-end'
+                    >
+                        <Button onClick={handleCloseDeleteConfirm}>
+                            Cancel
+                        </Button>
+                        <Button
+                            onClick={handleDelete}
+                            variant='contained'
+                            color='error'
+                        >
+                            Delete
+                        </Button>
+                    </Stack>
+                </Box>
+            </Drawer>
+        </>
     );
 }
 
