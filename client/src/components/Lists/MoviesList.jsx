b0c060026dea111c88769945292fe1a48b9662af
Borys Ziukov <ziukoff1985@gmail.com>
2026-02-09 14:43:51 +0200

    Fix deleting logic in MoviesList using ConfirmDrawer and useConfirm

--- client/src/components/Lists/MoviesList.jsx
+++ client/src/components/Lists/MoviesList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import {
@@ -14,9 +14,6 @@ import {
     Box,
     Tooltip,
     CircularProgress,
-    Drawer,
-    Button,
-    Stack,
 } from '@mui/material';
 import {
     Visibility as ViewIcon,
@@ -25,11 +22,10 @@ import {
     Person as PersonIcon,
 } from '@mui/icons-material';
 import { deleteMovie, getAllMovies } from '../../store/thunks/moviesThunks';
+import useConfirm from '../../hooks/useConfirm';
+import ConfirmDrawer from '../UI/ConfirmDrawer';
 
 function MoviesList() {
-    const [openConfirm, setOpenConfirm] = useState(false);
-    const [selectedMovieId, setSelectedMovieId] = useState(null);
-
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { movies, isPending } = useSelector((state) => state.moviesList);
@@ -38,28 +34,16 @@ function MoviesList() {
         dispatch(getAllMovies());
     }, [dispatch]);
 
-    const handleOpenDeleteConfirm = (id) => {
-        setOpenConfirm(true);
-        setSelectedMovieId(id);
+    const confirm = useConfirm();
+
+    const handleDeleteClick = (id) => {
+        confirm.openConfirm(id);
     };
 
-    const handleCloseDeleteConfirm = () => {
-        setOpenConfirm(false);
-        setSelectedMovieId(null);
+    const handleConfirmDelete = () => {
+        dispatch(deleteMovie(confirm.payload));
+        confirm.closeConfirm();
     };
-
-    const handleDelete = () => {
-        if (selectedMovieId) {
-            dispatch(deleteMovie(selectedMovieId));
-        }
-        handleCloseDeleteConfirm();
-    };
-
-    // const handleDelete = (id) => {
-    //     if (window.confirm('Are you sure you want to delete this movie?')) {
-    //         dispatch(deleteMovie(id));
-    //     }
-    // };
 
     if (!movies || isPending)
         return (
@@ -107,9 +91,7 @@ function MoviesList() {
                                         <Tooltip title='Delete'>
                                             <IconButton
                                                 onClick={() =>
-                                                    handleOpenDeleteConfirm(
-                                                        movie.id,
-                                                    )
+                                                    handleDeleteClick(movie.id)
                                                 }
                                                 color='error'
                                             >
@@ -161,44 +143,15 @@ function MoviesList() {
                 </List>
             </Paper>
 
-            <Drawer
-                anchor='right' // або 'bottom'
-                open={openConfirm}
-                onClose={handleCloseDeleteConfirm}
-            >
-                <Box
-                    sx={{
-                        width: { xs: '100vw', sm: 360 },
-                        p: 3,
-                    }}
-                >
-                    <Typography variant='h6' gutterBottom>
-                        ❗ Warning
-                    </Typography>
-
-                    <Typography variant='body2' color='text.secondary' mb={3}>
-                        Are you sure you want to delete? This action cannot be
-                        undone.
-                    </Typography>
-
-                    <Stack
-                        direction='row'
-                        spacing={2}
-                        justifyContent='flex-end'
-                    >
-                        <Button onClick={handleCloseDeleteConfirm}>
-                            Cancel
-                        </Button>
-                        <Button
-                            onClick={handleDelete}
-                            variant='contained'
-                            color='error'
-                        >
-                            Delete
-                        </Button>
-                    </Stack>
-                </Box>
-            </Drawer>
+            <ConfirmDrawer
+                open={confirm.open}
+                title='Delete movie'
+                description='Are you sure you want to delete movie? This action cannot be undone.'
+                confirmText='Delete'
+                cancelText='Cancel'
+                onConfirm={handleConfirmDelete}
+                onClose={confirm.closeConfirm}
+            />
         </>
     );
 }
